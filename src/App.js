07fa8e4594696fb079905554b596da557d3835d7
ninxi <stendert_baars@hotmail.com>
2020-04-22 15:46:56 +0200

    Add styling and change signup

--- src/App.js
+++ src/App.js
@@ -7,22 +7,17 @@ import Loading from "./components/Loading";
 import MessageBox from "./components/MessageBox";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Overview from "./pages/Overview"
+import Items from "./pages/Items"
+import Wishlist from "./pages/Wishlist"
+import Assign from "./pages/Assign"
+import Settings from "./pages/Settings"
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectAppLoading } from "./store/appState/selectors";
 import { getUserWithStoredToken } from "./store/user/actions";
-import { Jumbotron } from "react-bootstrap";
-
-const Home = () => (
-  <Jumbotron>
-    <h1>Home</h1>
-  </Jumbotron>
-);
-const Other = () => (
-  <Jumbotron>
-    <h1>Other</h1>
-  </Jumbotron>
-);
+import { Jumbotron, Nav } from "react-bootstrap";
+import { selectToken } from "../src/store/user/selectors"
 
 function App() {
   const dispatch = useDispatch();
@@ -31,17 +26,24 @@ function App() {
   useEffect(() => {
     dispatch(getUserWithStoredToken());
   }, [dispatch]);
+  const token = useSelector(selectToken)
+  // const navigationControl = token ? <Navigation /> : null
 
   return (
-    <div className="App">
+    <div className="App"><div><h1 className="banner">LOOTCOUNCIL.XX</h1></div>
       <Navigation />
+      {/* {navigationControl} */}
       <MessageBox />
       {isLoading ? <Loading /> : null}
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/other" component={Other} />
+        <Route exact path="/" component={Login} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
+        <Route path="/overview" component={Overview} />
+        <Route path="/items" component={Items} />
+        <Route path="/wishlist" component={Wishlist} />
+        <Route path="/assign" component={Assign} />
+        <Route path="/settings" component={Settings} />
       </Switch>
     </div>
   );
---
+++ src/pages/Assign/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from "react-bootstrap/Container"
+
+export default function Assign() {
+  return (
+
+    <Container>
+      <p>Assign</p>
+    </Container>
+
+  )
+}
