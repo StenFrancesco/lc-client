7271cea7dfea131af12a4cf3d639451ce08b6e24
ninxi <stendert_baars@hotmail.com>
2020-05-04 15:39:25 +0200

    install react-dnd react-dnd-html5-backend react-modal

--- src/pages/Assign/index.js
+++ src/pages/Assign/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from "react-bootstrap/Container"
+import Search from "../../components/Search"
 
 export default function Assign() {
   return (
 
     <Container>
       <p>Assign</p>
+      <Search />
     </Container>
 
   )
